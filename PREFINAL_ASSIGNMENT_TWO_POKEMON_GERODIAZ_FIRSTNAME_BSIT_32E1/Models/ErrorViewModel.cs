414fa48802e1a2ad10bea622a28013eacaadda46
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-05-11 19:07:25 +0800

    Add project files.

---
+++ PREFINAL_ASSIGNMENT_TWO_POKEMON_GERODIAZ_FIRSTNAME_BSIT_32E1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_GERODIAZ_FIRSTNAME_BSIT_32E1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
