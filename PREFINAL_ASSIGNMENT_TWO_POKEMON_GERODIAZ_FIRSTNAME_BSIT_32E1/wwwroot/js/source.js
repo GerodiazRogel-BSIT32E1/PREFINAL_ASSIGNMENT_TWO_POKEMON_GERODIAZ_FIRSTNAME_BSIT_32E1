2961d41a3faedc7a02b89540df8ce6223e00a8a7
GerodiazRogel-BSIT32E1 <gerodiazrogel1@gmail.com>
2024-05-25 22:56:35 +0800

    adding a home page text image and some animation

--- PREFINAL_ASSIGNMENT_TWO_POKEMON_GERODIAZ_FIRSTNAME_BSIT_32E1/wwwroot/js/source.js
+++ PREFINAL_ASSIGNMENT_TWO_POKEMON_GERODIAZ_FIRSTNAME_BSIT_32E1/wwwroot/js/source.js
@@ -1,10 +1,15 @@
 const toggle = document.querySelector(".dark-mode-toggle img");
 let originalSrc = toggle.getAttribute("src");
+let alternateSrc = toggle.getAttribute("data-src");
+
+// Set default mode to dark mode
+document.body.classList.add("dark-mode");
+toggle.src = alternateSrc;
 
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
   if (document.body.classList.contains("dark-mode")) {
-    toggle.src = toggle.getAttribute("data-src");
+    toggle.src = alternateSrc;
   } else {
     toggle.src = originalSrc;
   }
